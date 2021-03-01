f0c3c13a63b16ad14435028ecee72be9d97d1bfe
thisiskeanyvy <kinvestisseur@gmail.com>
2021-03-01 14:00:32 +0100

    debug du script de détection d'os js

--- assets/js/os.js
+++ assets/js/os.js
@@ -5,14 +5,14 @@ if (navigator.userAgent.indexOf("Linux") != -1) OS =  "Linux";
 if (navigator.userAgent.indexOf("Android") != -1) OS =  "Android"; 
 if (navigator.userAgent.indexOf("like Mac") != -1) OS =  "iOS"; 
 
-if (OS = "Linux") {
+if (OS == "Linux") {
 	document.getElementById("download-tracker").innerHTML = '<li class="cta-button disable-select"><a href="https://github.com/likapi/tracker/releases/download/v1.0-beta/v1.0-beta-likapi.deb" target="_blank">Télécharger</a></li>';
 }
 
-if (OS = "Windows") {
+if (OS == "Windows") {
 	document.getElementById("download-tracker").innerHTML = '<li class="cta-button disable-select"><a href="https://github.com/likapi/tracker" target="_blank">Télécharger</a></li>';
 }
 
-if (OS = "Macintosh") {
+if (OS == "Macintosh") {
 	document.getElementById("download-tracker").innerHTML = '<li class="cta-button disable-select"><a href="https://github.com/likapi/tracker" target="_blank">Télécharger</a></li>';
 }
